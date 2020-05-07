607aaf9d75c4068ad46f40f3345c68f7b7a234f7
Paul Luu <paul_luu.work@hotmail.com>
2020-05-06 22:34:33 -0400

    Adding input box

--- src/player.ts
+++ src/player.ts
@@ -1,9 +1,10 @@
 import "phaser";
 import MultiKey from "./multi-key.js";
-import { FlatBoy } from "./flatboy.js";
+import { isOpened } from './ui';
+import MyGame from "./game.js";
 
 export default class Player {
-    scene : Phaser.Scene;
+    scene : MyGame;
     sprite : Phaser.Physics.Matter.Sprite;
     canJump : Boolean;
     jumpCooldownTimer: Phaser.Time.TimerEvent;
@@ -46,54 +47,56 @@ export default class Player {
 
   update() {
     if (this.destroyed) return;
+  
+    if(!this.scene.isTyping){
+      const sprite : any = this.sprite;
+      const velocity = sprite.body.velocity;
+      const isRightKeyDown = this.rightInput.isDown();
+      const isLeftKeyDown = this.leftInput.isDown();
+      const isJumpKeyDown = this.jumpInput.isDown();
+      const isAttackKeyDown = this.attackInput.isDown();
+      const isThrowKeyDown = this.throwInput.isDown();
+      const isOnGround = sprite.isTouching.ground;
+      const isInAir = !isOnGround;
 
-    const sprite : any = this.sprite;
-    const velocity = sprite.body.velocity;
-    const isRightKeyDown = this.rightInput.isDown();
-    const isLeftKeyDown = this.leftInput.isDown();
-    const isJumpKeyDown = this.jumpInput.isDown();
-    const isAttackKeyDown = this.attackInput.isDown();
-    const isThrowKeyDown = this.throwInput.isDown();
-    const isOnGround = sprite.isTouching.ground;
-    const isInAir = !isOnGround;
+      // --- Move the player horizontally ---
 
-    // --- Move the player horizontally ---
+      // Adjust the movement so that the player is slower in the air
+      const moveForce = isOnGround ? 0.e01 : 0.005;
 
-    // Adjust the movement so that the player is slower in the air
-    const moveForce = isOnGround ? 0.e01 : 0.005;
+      if (isInAir) {
+        
+      }
+      else if (isAttackKeyDown){
+        sprite.animate(sprite.name + '-jump');
+      }
+      else if (isThrowKeyDown){
+        sprite.animate(sprite.name + '-throw');
+      }
+      else if (isLeftKeyDown === isRightKeyDown) {
+        sprite.setVelocityX(0);
+        sprite.animate(sprite.name +'-idle');
+      }
+      else if (isLeftKeyDown) {
+        sprite.animate(sprite.name + '-run');
+        sprite.setFlipX(true);
+        sprite.setVelocityX(-7)
 
-    if (isInAir) {
+      } else if (isRightKeyDown) {
+        sprite.animate(sprite.name + '-run');
+        sprite.setFlipX(false);
+        sprite.setVelocityX(7)
+      }
+
+      if (velocity.x > 7) sprite.setVelocityX(7);
+      else if (velocity.x < -7) sprite.setVelocityX(-7);
+
       
-    }
-    else if (isAttackKeyDown){
-      sprite.animate(sprite.name + '-jump');
-    }
-    else if (isThrowKeyDown){
-      sprite.animate(sprite.name + '-throw');
-    }
-    else if (isLeftKeyDown === isRightKeyDown) {
-      sprite.setVelocityX(0);
-      sprite.animate(sprite.name +'-idle');
-    }
-    else if (isLeftKeyDown) {
-      sprite.animate(sprite.name + '-run');
-      sprite.setFlipX(true);
-      sprite.setVelocityX(-7)
-
-    } else if (isRightKeyDown) {
-      sprite.animate(sprite.name + '-run');
-      sprite.setFlipX(false);
-      sprite.setVelocityX(7)
-    }
-
-    if (velocity.x > 7) sprite.setVelocityX(7);
-    else if (velocity.x < -7) sprite.setVelocityX(-7);
-
-    
-    if (isJumpKeyDown && sprite.canJump && isOnGround) {
-      sprite.isOnGround = false;
-      sprite.setVelocityY(-11);
-      sprite.animate(sprite.name + '-jump');
+      if (isJumpKeyDown && sprite.canJump && isOnGround) {
+        sprite.isOnGround = false;
+        sprite.setVelocityY(-11);
+        sprite.animate(sprite.name + '-jump');
+      }
     }
   }
 
