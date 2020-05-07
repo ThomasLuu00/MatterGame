11131ba56d15af9319430fede0edf1a7c3d38399
ThomasLuu00 <Thomas.Luu00@gmail.com>
2020-05-07 17:55:16 -0400

    Added projectiles

---
+++ src/projectile.ts
@@ -0,0 +1,24 @@
+export default class Projectile extends Phaser.Physics.Matter.Sprite{
+    speed = 1;
+
+    constructor (world : Phaser.Physics.Matter.World, x : number = 0, y : number = 0, anim: string)
+    {
+        super(world, x, y, anim, 0);
+    }
+
+    shoot(target: Phaser.Geom.Point)
+    {
+        let direction = Math.atan( (target.x-this.x) / (target.y-this.y));
+
+        // Calculate X and y velocity of bullet to moves it from shooter to target
+        let mod: number = (target.y >= this.y) ? 1 : -1
+
+        let xRatio: number = mod * Math.sin(direction);
+        let yRatio: number = mod * Math.cos(direction);
+        let xSpeed: number = this.speed * xRatio;
+        let ySpeed: number  = this.speed * yRatio;
+
+        this.rotation = Phaser.Math.Angle.Between(this.x, this.y, target.x, target.y) + Phaser.Math.DegToRad(90);
+        this.setVelocity(xSpeed, ySpeed)
+    }
+};
