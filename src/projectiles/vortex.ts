3491976b494ae7f20a73c3d47eb8ff99fabc5aca
ThomasLuu00 <Thomas.Luu00@gmail.com>
2020-05-13 16:05:15 -0400

    Added vortex projectile

---
+++ src/projectiles/vortex.ts
@@ -0,0 +1,48 @@
+import ProjectileBase from './projectile-base';
+import { ProjectileData, projectileList } from '../item/item-data';
+import { Particle } from './particles';
+
+
+export default class VortexProjectile extends ProjectileBase{
+
+    speed = 10;
+
+    setSprite(x: number, y: number): void{
+        this.sprite = this.scene.matter.add.sprite(x, y, 'vortex', 0);
+    }
+
+    setData(): void {
+        this.projectileData = projectileList.P01000;
+    }
+
+    onFire(x: number, y: number): void {
+        const sprite = this.sprite;
+        const direction = Math.atan((x - sprite.x) / (y - sprite.y));
+        // Calculate X and y velocity of bullet to moves it from shooter to target
+        const mod: number = y >= sprite.y ? 1 : -1;
+
+        const xRatio: number = mod * Math.sin(direction);
+        const yRatio: number = mod * Math.cos(direction);
+        const xSpeed: number = this.speed * xRatio;
+        const ySpeed: number = this.speed * yRatio;
+
+        sprite.rotation = Phaser.Math.Angle.Between(sprite.x, sprite.y, x, y) + Phaser.Math.DegToRad(90);
+        sprite.setIgnoreGravity(true);
+        sprite.setFrictionAir(0);
+        sprite.setVelocity(xSpeed, ySpeed);
+        sprite.play('vortex');
+    }
+
+    onHit(event: any): void {
+        //
+    }
+
+    onUpdate(event: any): void {
+        //
+    }
+
+    onDestroy(event: any): void {
+        //
+    }
+
+}
